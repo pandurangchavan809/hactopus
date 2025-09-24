da1e44fba13156970c90f4778466628f15fb6033
pandurangchavan809 <pandurangchavan809@gmail.com>
2025-09-24 16:09:27 +0530

    resolve

---
+++ frontend/firebase-config.js
@@ -0,0 +1,7 @@
+// frontend/firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyAM7d_jAB1jwyGL9JlUmkhtH6uJybLZ8OY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "hactopus-612e9",
+  // ... other keys as shown in your Firebase console
+};
